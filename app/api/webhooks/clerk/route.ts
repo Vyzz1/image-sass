92214eeb9925badabc4578b69865b2ebe2d1cc26
VyHuynh <vykhanghuynh@gmail.com>
2024-06-14 14:59:31 +0700

    Refactor webhook route to use object shorthand syntax

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -62,7 +62,7 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
-    const user: CreateUserParams = {
+    const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
@@ -89,7 +89,7 @@ export async function POST(req: Request) {
   if (eventType === "user.updated") {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
-    const user: UpdateUserParams = {
+    const user = {
       firstName: first_name,
       lastName: last_name,
       username: username!,
